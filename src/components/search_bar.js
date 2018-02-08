33258a5b7a96b19e9008de9f06e274565f55cb54
Norman Chan <slowmanchan@gmail.com>
2018-02-08 10:25:34 -0500

    tut

---
+++ src/components/search_bar.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+// class based component
+// start with functional comp. first and then refactor
+// to class based comp. (if you need more functionanilty
+// )
+class SearchBar extends Component {
+  // all js objects have a constructor function
+  // is called first when new instance is initialized
+  constructor(props) {
+    super(props);
+
+    this.state = { term: '' };
+  }
+  // render is called evertime setState is called
+  // rerendering things (like <input /> s)
+  render() {
+
+    return (
+      <div>
+        <input
+          value={this.state.term}
+          onChange={e => this.setState({ ...this.state, term: e.target.value })}
+        />
+            Value of the input: {this.state.term}
+      </div>
+    );
+  }
+
+
+  // handle user input with an event handler (eg onChange)
+  // html elements emit an event object when user does something
+  // wrap jsx with {this.onInputChange}
+}
+
+export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,40 @@
-// Create a new component.  This should produce some html.
+// Core react library for components (will not render to dom)
+// need react-dom lib to render things to the dom
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+import VideoList from './components/video_list';
+import SearchBar from './components/search_bar';
+// Youtube api key
+const API_KEY = 'AIzaSyDtWMSttWfPJL8GoEjwBIsIu1dQeNLiR24';
 
+
+
+// Create a new component.  This should produce some html.
+// This is a component class (not an instance)
+// es6 arrow function
+class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = { videos: [] };
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({ videos });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+      <VideoList videos={this.state.videos}/>
+      </div>
+    );
+  }
+}
 // Take this components' generated HTML and put it in the DOM.
+// Pass an instance (not the class itself) of the component to render it.
+// Wrap it with <App /> or <App > <App /> to create an instance.
+// ReactDOM.render takes two args (component and where in the html to put the comp)
+ReactDOM.render(<App />, document.querySelector('.container'));
